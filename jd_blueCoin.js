1bf0f437b339472adb6f43cc5ff2dfec6439f9e6
lb <346467389@qq.com>
2021-06-28 13:45:27 +0800

    12

--- jd_blueCoin.js
+++ jd_blueCoin.js
@@ -25,7 +25,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 let allMessage = '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let coinToBeans = $.getdata('coinToBeans') || 0; //兑换多少数量的京豆（20或者1000），0表示不兑换，默认不兑换京豆，如需兑换把0改成20或者1000，或者'商品名称'(商品名称放到单引号内)即可
+let coinToBeans = $.getdata('coinToBeans') || 1000; //兑换多少数量的京豆（20或者1000），0表示不兑换，默认不兑换京豆，如需兑换把0改成20或者1000，或者'商品名称'(商品名称放到单引号内)即可
 let jdNotify = false;//是否开启静默运行，默认false关闭(即:奖品兑换成功后会发出通知提示)
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
@@ -131,6 +131,35 @@ async function PrizeIndex() {
         console.log(`兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`);
         $.beanerr = `兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`;
       }
+	  //1000结束兑换20豆
+	  if($.beanscount==0){
+		  //1000豆子兑换失败，开始兑换20豆子
+		  if (prizeList[1] && prizeList[1].type === 3) {
+			console.log(`查询换${prizeList[1].name}ID成功，ID:${prizeList[1].prizeId}`)
+			$.title = prizeList[1].name;
+			$.blueCost = prizeList[1].cost;
+		  } else {
+			console.log(`查询换万能的京豆ID失败`)
+			$.beanerr = `东哥今天不给换`;
+			return ;
+		  }
+		  if (prizeList[0] && prizeList[0].inStock === 506) {
+			console.log(`失败，万能的京豆领光了，请明天再来`);
+			$.beanerr = `失败，万能的京豆领光了，请明天再来`;
+			return ;
+		  }
+		  if ((prizeList[0] && prizeList[0].limit) === (prizeList[0] && prizeList[0].finished)) {
+			$.beanerr = `${prizeList[0].name}`;
+			return ;
+		  }
+		  //兑换万能的京豆(1-20京豆)
+		  if ($.totalBlue > $.blueCost) {
+			await smtg_obtainPrize(prizeList[0].prizeId,1000);
+		  } else {
+			console.log(`兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`);
+			$.beanerr = `兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`;
+		  }
+	  }
     } else if (`${coinToBeans}` === '20') {
       if (prizeList[1] && prizeList[1].type === 3) {
         console.log(`查询换${prizeList[1].name}ID成功，ID:${prizeList[1].prizeId}`)
