3457ae138f4f2c8f29bcf544dec680febae9f0bc
libin <346467389@qq.com>
2022-06-20 01:40:53 +0800

    1

--- jd_heilongjiang.js
+++ jd_heilongjiang.js
@@ -29,7 +29,7 @@ var isOK=true;
 		isOK=true;
 		totallMessage=``;
 		for(var i=0;i<cookies.length;i++){
-			totallMessage+=totallMessage+`开始第"+(i+1)+"个账号\n`;
+			totallMessage+=totallMessage+`开始第`+(i+1)+`个账号\n`;
 			cookie=cookies[i];
 			await startGame();
 			time=12+Math.floor(Math.random()*3);//0-3
