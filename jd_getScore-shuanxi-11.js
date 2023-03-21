4eba00fb4e83c383fc12cce77004e5826f558f7c
libin <346467389@qq.com>
2023-03-21 17:18:44 +0800

    111

---
+++ jd_getScore-shuanxi-11.js
@@ -0,0 +1,289 @@
+/*
+
+cron "0-59/5 * * * *" jd_getScore-shuanxi-gq.js, tag:双喜过期
+ */
+const $ = new Env('双喜过期');
+
+
+const fs = require('fs');
+const CryptoJS=require('crypto-js');
+let isEnd=false;
+
+
+let options = {
+ flags: 'a', // 
+ encoding: 'utf8', // utf8编码
+}
+let stderr = fs.createWriteStream('./shuanxi.log', options);
+let logger = new console.Console(stderr);
+
+
+let isLogin=true;
+
+let cookies=[
+	"springSession=4e29dada-d2ff-402f-ab1f-f7e993849733",//leebear
+	"springSession=8cbb1c1c-847e-41d4-bcba-565b3e7e1d0c",//天天快乐
+	"springSession=47363a87-a307-4e02-afb6-68b4e5ae00c3",//bona
+	"springSession=4253e3f9-f918-475f-892e-3ffdd69b7f1c",//liushufang
+	"springSession=5e96ee89-6667-4f42-93af-68eddcae3d34",//001
+	"springSession=1f4ef42f-142b-4684-b208-83f0f30ce4b5",//002
+	"springSession=87bf7971-6fe3-4a07-ad99-9f0a57484690",//003
+	"springSession=d1c3d3d5-fed9-443a-876e-7381406ebb4e",//004
+	"springSession=f12ce144-73ae-4a96-a0a5-f926cb9b2031",//006
+	"springSession=740ed466-970b-4e17-aa7a-31f88f8544ac",//009
+	"springSession=977678d8-4b45-46bf-9c83-bda4be20f317",//010
+	"springSession=7902e332-2151-4b34-948a-75dec794722f",//慢慢
+
+];
+let cookie='';
+!(async () => {
+
+	
+	
+
+	/*for(var i=0;i<cookies.length;i++){
+		cookie=cookies[i];
+		$.index=i;
+		await getFirstScore();
+		await $.wait(2000);
+	}*/
+	var i=0;
+	do{
+		cookie=getSession();
+		i++;
+		$.index=i;
+		await getFirstScore();
+		await $.wait(2000);
+		
+	}while(1==1)
+
+	console.log("\n游戏结束\n")
+})()
+  .catch((e) => {
+    $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
+  })
+  .finally(() => {
+    $.done();
+  })
+function startGame(){ 
+	console.log(getNowFormatDate()+"开始游戏》》》");
+	return new Promise((resolve, reject) => {
+		let option = taskurl(`https://guangdong-tobacco.venubao.com/game/play/start/22`);
+		//console.log(option);
+		$.get(option, (err, resp, data) => {
+			try {
+				if (err) {
+					console.log(` API请求失败，请检查网路\n${JSON.stringify(err)}`)
+				} else {
+					data = JSON.parse(data)
+					//console.log(data)
+					if (data.code==0) {//成功
+						$.key=data.data.key;
+						$.periodId=data.data.periodId;
+						$.playId=data.data.playId
+						$.startTimeStr=data.data.startTimeStr;
+						console.log(getNowFormatDate()+"获取信息成功》》》");
+						console.log(data.data);
+					} else {
+						isLogin=false;
+						console.log(getNowFormatDate()+"💩 获得列表失败:"+data);
+					}
+				}
+			} catch (e) {
+				reject(`API返回结果解析出错\n${e}\n${JSON.stringify(data)}`)
+			} finally {
+				resolve()
+			}
+		})
+	})
+}
+function endGame(){
+	
+	blockcurSc($.nowGrade);
+	let body='{"gameType":1,"playId":'+$.playId+',"periodId":'+$.periodId+',"grade":"'+$.grade+'","key":"'+$.key+'"}';
+	const myRequest = getPostRequest("https://guangdong-tobacco.venubao.com/game/play/end", body);
+	//console.log(myRequest)
+	return new Promise(resolve => {
+		$.post(myRequest, (err, resp, data) => {
+		  try {
+			console.log(getNowFormatDate()+"成绩提交");
+			data = JSON.parse(data);
+			console.log(data);
+			isEnd=true;
+		  } catch (e) {
+			$.logErr(e, resp)
+		  } finally {
+			resolve();
+		  }
+		})
+    })
+}
+function getFirstScore(){
+	//获取第一名成绩
+	return new Promise((resolve, reject) => {
+		let option = taskurl(`https://guangdong-tobacco.venubao.com/userwx/getUser`)
+		$.get(option, (err, resp, data) => {
+			try {
+				if (err) {
+					console.log(` API请求失败，请检查网路\n${JSON.stringify(err)}`)
+				} else {
+					//console.log(data)
+					console.log("当前账号sessionId："+cookie)
+					data = JSON.parse(data)
+					//console.log(data)
+					if (data.return_code==0) {//成功
+					    var res = JSON.parse(data.result);
+						console.log(res.points);
+						logger.log("当前账号sessionId"+cookie+",积分:"+res.points);
+						//$.firstScore = data.data.rankingList[0].grade;
+						//$.firstScore =200000;
+						console.log(getNowFormatDate()+`💩 获取成功`)
+					} else {
+						console.log(getNowFormatDate()+`💩 获取失败: ${data.result}`)
+					}
+				}
+			} catch (e) {
+				reject(`API返回结果解析出错\n${e}\n${JSON.stringify(data)}`)
+			} finally {
+				resolve()
+			}
+		})
+	})
+}
+function getNowScore(){
+	var now=new Date();
+	var startTime=new Date($.startTimeStr);
+	$.nowGrade=Math.floor((now.getTime()-startTime.getTime())/1000)*100;
+	console.log(getNowFormatDate()+"目前第一名成绩："+$.firstScore+"分,当前游戏分数："+$.nowGrade);
+}
+function sleep(time) {
+    var startTime = new Date().getTime() + parseInt(time, 10);
+    while(new Date().getTime() < startTime) {}
+};
+function blockcurSc(score) {
+	var t = yyyymmdd(),
+	e = getMonthDays(),
+	i = $.key ? $.key.length: 0,
+	o = "00000000" + parseInt(t * e / i),
+	n = String(score),
+	r = CryptoJS.enc.Utf8.parse(o),
+	a = CryptoJS.enc.Utf8.parse(o),
+	s = CryptoJS.AES.encrypt(n, r, {
+		iv: a,
+		mode: CryptoJS.mode.CBC,
+		padding: CryptoJS.pad.ZeroPadding
+	}),
+	c = CryptoJS.enc.Base64.stringify(s.ciphertext);
+	$.grade=c;
+	console.log("\u52a0\u5bc6" + c);
+	var h = CryptoJS.enc.Base64.parse(c),
+	l = CryptoJS.enc.Base64.stringify(h),
+	u = CryptoJS.AES.decrypt(l, r, {
+		iv: a,
+		mode: CryptoJS.mode.CBC,
+		padding: CryptoJS.pad.ZeroPadding
+	}).toString(CryptoJS.enc.Utf8);
+	return console.log("\u89e3\u5bc6" + u),
+	c
+}
+function yyyymmdd() {
+	var t = new Date,
+	e = t.getFullYear().toString(),
+	i = (t.getMonth() + 1).toString(),
+	o = t.getDate().toString();
+	return e + (i[1] ? i: "0" + i[0]) + (o[1] ? o: "0" + o[0])
+}
+function getMonthDays() {
+	var t = new Date,
+	e = t.getFullYear(),
+	i = t.getMonth() + 1;
+	return new Date(e, i, 0).getDate()
+}
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+	var hours=date.getHours();
+	var minutes=date.getMinutes();
+	var seconds=date.getSeconds();
+	if (hours >= 0 && hours <= 9) {
+        hours = "0" + hours;
+    }
+	if (minutes >= 0 && minutes <= 9) {
+        minutes = "0" + minutes;
+    }
+	if (seconds >= 0 && seconds <= 9) {
+        seconds = "0" + seconds;
+    }
+	
+	var milliseconds=date.getMilliseconds();
+
+	if (milliseconds >= 0 && milliseconds <= 9) {
+        milliseconds = "00" + milliseconds;
+    }
+	if (milliseconds >= 10 && milliseconds <= 99) {
+        milliseconds = "0" + milliseconds;
+    }
+	
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+            + " " + hours + seperator2 + minutes
+            + seperator2 + seconds+"."+milliseconds;
+    return currentdate;
+}
+function taskurl(url) {
+	return {
+		'url': url,
+		'headers': {
+			'Host': 'guangdong-tobacco.venubao.com',
+			'Accept-Encoding': 'gzip, deflate, br',
+			'Cookie': cookie,
+			'Connection': 'keep-alive',
+			'Accept': '*/*',
+			'UserAgent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+			'Accept-Language': 'zh-cn',
+			'Referer': 'https://guangdong-tobacco.venubao.com/guangdong/activity/watermelon/h5/rank.html'
+		},
+	}
+}
+function getPostRequest(url, body) {
+  const method = `POST`;
+  const headers = {
+    'Accept-Encoding': `gzip, deflate, br`,
+	'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Host': `guangdong-tobacco.venubao.com`,
+    'Origin': `https://guangdong-tobacco.venubao.com`,
+    'Connection': `keep-alive`,
+    'biz-type': `service-monitor`,
+    'Accept-Language': `zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7`,
+    'Content-Type': `application/json;charset=UTF-8`,
+    "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"),
+    'Referer': `https://guangdong-tobacco.venubao.com/guangdong/activity/watermelon/index.html`,
+    'X-Requested-With': `com.tencent.mm`,
+    'Cookie': cookie
+  };
+  return {url: url, method: method, headers: headers, body: body};
+}
+function getSession(){
+	var len=32;
+	var chars="abcdefg1234567890";
+	var amx=chars.length;
+	var pwd="springSession=";
+	for(var i=0;i<len;i++){
+		pwd+=chars.charAt(Math.floor(Math.random()*amx));
+		if(i==11||i==15||i==19){
+			pwd+="-"
+		}
+	}
+	//pwd="springSession=4e29dada-d2ff-402f-ab1f-f7e993849733"
+	return pwd;
+}
+// prettier-ignore
+function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
