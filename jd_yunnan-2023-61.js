fef4031caeb58742264d82149d76b5dabd426fd2
libin <346467389@qq.com>
2023-06-14 09:05:14 +0800

    1

--- jd_yunnan-2023-61.js
+++ jd_yunnan-2023-61.js
@@ -263,10 +263,10 @@ function choujiang(){//抽奖
 	return new Promise(async resolve => {
 	var questionKey=$.keyList;
 	var now=new Date();
-	console.log(now);
+	console.log(now.toLocaleTimeString());
 	if(now.getMinutes()==1&&(now.getHours()==9||now.getHours()==14)){
 		console("停止");
-		break;
+		return;
 	}
 	var dat="wx_tk="+$.wx_tk+"&saidao=D&p_school=呈贡区&p_area=昆明市&answer["+questionKey[0]+"]="+questionJson[questionKey[0]].anw+"&answer["+questionKey[1]+"]="+questionJson[questionKey[1]].anw+"&answer["+questionKey[2]+"]="+questionJson[questionKey[2]].anw+"&answer["+questionKey[3]+"]="+questionJson[questionKey[3]].anw+"&answer["+questionKey[4]+"]="+questionJson[questionKey[4]].anw;
 	var str=dat+"&schoolData[]=云南大学&schoolData[]=昆明理工大学&schoolData[]=云南师范大学&schoolData[]=昆明医科大学&schoolData[]=云南农业大学&schoolData[]=云南中医药大学&schoolData[]=云南民族大学&schoolData[]=云南财经大学&schoolData[]=西南林业大学&schoolData[]=云南艺术学院&schoolData[]=云南警官学院&schoolData[]=云南国防工业职业技术学院&schoolData[]=昆明学院&schoolData[]=大理大学&schoolData[]=玉溪师范学院&schoolData[]=曲靖师范学院&schoolData[]=楚雄师范学院&schoolData[]=红河学院&schoolData[]=保山学院&schoolData[]=文山学院&schoolData[]=昭通学院&schoolData[]=普洱学院&schoolData[]=滇西科技师范学院&schoolData[]=滇西应用技术大学&schoolData[]=云南大学滇池学院&schoolData[]=昆明理工大学津桥学院&schoolData[]=昆明城市学院&schoolData[]=昆明文理学院&schoolData[]=昆明医科大学海源学院&schoolData[]=云南艺术学院文华学院&schoolData[]=丽江文化旅游学院&schoolData[]=云南工商学院&schoolData[]=云南经济管理学院&schoolData[]=昆明冶金高等专科学校&schoolData[]=云南交通职业技术学院&schoolData[]=云南机电职业技术学院&schoolData[]=云南国土资源职业学院&schoolData[]=云南林业职业技术学院&schoolData[]=云南农业职业技术学院&schoolData[]=昆明工业职业技术学院&schoolData[]=云南司法警官职业学院&schoolData[]=云南文化艺术职业学院&schoolData[]=云南体育运动职业技术学院&schoolData[]=德宏师范高等专科学校&schoolData[]=丽江师范高等专科学校&schoolData[]=云南旅游职业学院&schoolData[]=曲靖医学高等专科学校&schoolData[]=楚雄医药高等专科学校&schoolData[]=保山中医药高等专科学校&schoolData[]=西双版纳职业技术学院&schoolData[]=云南能源职业技术学院&schoolData[]=玉溪农业职业技术学院&schoolData[]=云南锡业职业技术学院&schoolData[]=德宏职业学院&schoolData[]=红河卫生职业学院&schoolData[]=大理农林职业技术学院&schoolData[]=云南财经职业学院&schoolData[]=昆明铁道职业技术学院&schoolData[]=昭通卫生职业学院&schoolData[]=大理护理职业学院&schoolData[]=云南水利水电职业学院&schoolData[]=云南轻纺职业学院&schoolData[]=云南特殊教育职业学院&schoolData[]=云南工贸职业技术学院&schoolData[]=云南交通运输职业学院&schoolData[]=昆明幼儿师范高等专科学校&schoolData[]=曲靖职业技术学院&schoolData[]=红河职业技术学院&schoolData[]=云南现代职业技术学院&schoolData[]=云南科技信息职业学院&schoolData[]=昆明艺术职业学院&schoolData[]=云南城市建设职业学院&schoolData[]=云南新兴职业学院&schoolData[]=云南工程职业学院&schoolData[]=云南经贸外事职业学院&schoolData[]=云南三鑫职业技术学院&schoolData[]=云南商务职业学院&schoolData[]=昆明卫生职业学院&schoolData[]=云南外事外语职业学院&schoolData[]=云南医药健康职业学院&schoolData[]=云南理工职业学院";
