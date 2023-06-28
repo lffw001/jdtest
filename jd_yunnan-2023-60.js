a557eb7b83882f07223e62742f7f39d3ed492c69
libin <346467389@qq.com>
2023-06-28 16:16:40 +0800

    1

--- jd_yunnan-2023-60.js
+++ jd_yunnan-2023-60.js
@@ -149,8 +149,7 @@ function getRandomArrayElements(arr, count) {
         shuffled[index] = shuffled[i];
         shuffled[i] = temp;
     }
-    //return shuffled.slice(min);
-	return [ 'q206', 'q207', 'q208', 'q209', 'q210' ]
+    return shuffled.slice(min);
 }
 
 function answer(key,que) {
